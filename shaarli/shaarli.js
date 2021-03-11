1601cb118d0d693e9bfd839d8892d9c7c7d1e949
GregThib <GregThib@users.noreply.github.com>
2021-03-11 23:49:14 +0100

    [IMP] aesthetic things in code

--- shaarli/shaarli.js
+++ shaarli/shaarli.js
@@ -1,6 +1,6 @@
 function shaarli(id) {
-    try {
-		xhr.json("backend.php",	
+	try {
+		xhr.json("backend.php",
 				{
 					'op': 'pluginhandler',
 					'plugin': 'shaarli',
@@ -11,14 +11,14 @@ function shaarli(id) {
 					if (reply) {
 						var share_url = reply.shaarli_url + "?post="+ encodeURIComponent(reply.link)+ "&title="+encodeURIComponent(reply.title)+"&source=bookmarklet";
 						console.log(share_url);
-						window.open( share_url, 
+						window.open( share_url,
 									'_blank',
 									'menubar=no,height=390,width=600,toolbar=no,scrollbars=no,status=no,dialog=1' );
 					} else {
 						Notify.error("<strong>Error encountered while initializing the Shaarli Plugin!</strong>", true);
 					}
 				});
-    } catch (e) {
-	Notify.error("Shaarli", e);
-    }
+	} catch (e) {
+		Notify.error("Shaarli", e);
+	}
 }
